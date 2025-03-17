5150ad35e14080e4f27c6bf091804c617670f0af
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2025-03-17 17:47:34 +0100

    acabado

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 import psycopg
 from psycopg.rows import dict_row
+import os
 
 def get_connection():
     try:
@@ -13,13 +14,13 @@ def get_connection():
              row_factory=dict_row # type: ignore
          )"""
          conn = psycopg.connect(
-             host="localhost",
-             port=5432,
-             dbname="picotrake",      
-             user="andres",
-             password="1234",
-             row_factory=dict_row # type: ignore
-         )
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")), #type: ignore
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            row_factory=dict_row
+        )
          return conn
     except psycopg.Error as e:
         raise HTTPException(status_code=500, detail=f"Error de conexión a la base de datos: {str(e)}")
--- dockerfile
+++ dockerfile
@@ -11,4 +11,4 @@ RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["uvicorn", "API.main:app", "--host", "0.0.0.0", "--port", "443", "--ssl-keyfile", "/app/ssl/key.pem", "--ssl-certfile", "/app/ssl/cert.pem"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "443", "--ssl-keyfile", "/app/ssl/key.pem", "--ssl-certfile", "/app/ssl/cert.pem"]
