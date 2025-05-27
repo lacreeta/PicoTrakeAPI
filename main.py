5bd2787d20a09c3413555df4cea785990da08ffa
Andrés González <2023_andres.gonzalez@iticbcn.cat>
2025-05-27 21:01:09 +0200

    modificado cors ahora si

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ app.add_middleware(
 # nginx
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["picotrake.com", "api.picotrakeclub.tech", "localhost", "127.0.0.1", "picotrakeapi-production.up.railway.app"]
+    allowed_hosts=["api.picotrake.com", "api.picotrakeclub.tech", "localhost", "127.0.0.1", "picotrakeapi-production.up.railway.app"]
 )
 
 # Root
