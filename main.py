78f974088a5cc9cd3d0e925a0a824247f8f346f1
Andrés González <2023_andres.gonzalez@iticbcn.cat>
2025-05-27 19:26:17 +0200

    AAAA

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ app.openapi = custom_openapi
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
