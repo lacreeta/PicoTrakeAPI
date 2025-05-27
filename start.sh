47a899249f84a3f7307ce8e7c2eb5026454461eb
Andrés González <2023_andres.gonzalez@iticbcn.cat>
2025-05-27 19:13:08 +0200

    subido start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+# !/bin/sh
+
+# HTTPS
+# uvicorn main:app --host 0.0.0.0 --port 443 \
+#     --ssl-keyfile /app/ssl/key.pem \
+#     --ssl-certfile /app/ssl/cert.pem &
+
+# HTTP
+uvicorn main:app --host 0.0.0.0 --port 8000
