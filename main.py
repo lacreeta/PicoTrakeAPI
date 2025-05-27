2bb0d62bdf4950c4a2fe1fe915ef6aa666bac8af
Andrés González <2023_andres.gonzalez@iticbcn.cat>
2025-05-27 20:47:41 +0200

    modificao pal cors

--- main.py
+++ main.py
@@ -67,7 +67,11 @@ app.openapi = custom_openapi
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://picotrake.com",
+        "https://www.picotrake.com",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -76,7 +80,7 @@ app.add_middleware(
 # nginx
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["picotrake.com, api.picotrakeclub.tech", "localhost", "127.0.0.1", "picotrakeapi-production.up.railway.app"]
+    allowed_hosts=["picotrake.com", "api.picotrakeclub.tech", "localhost", "127.0.0.1", "picotrakeapi-production.up.railway.app"]
 )
 
 # Root
