273ed33bbb1476e7f7e8949e01ce2e857f75ea68
Andrés González <2023_andres.gonzalez@iticbcn.cat>
2025-05-27 20:52:53 +0200

    modificado cors ahora si

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ app.add_middleware(
         "https://www.picotrake.com",
         "http://localhost:5173"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
